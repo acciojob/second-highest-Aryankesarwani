3a35e26d1281365fb837fd40e664b5a7a9b2b0fb
Aryan Kesarwani <93967099+Aryankesarwani@users.noreply.github.com>
2025-09-19 18:17:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
 //your JS code here. If required.
 secondHighest(arr){
 	
-	let highest=-Infinity;
-	let second=-Infinity;
-	for (let x of arr) {
-	    if (x > highest) {
-	      second = highest;
-	      highest = x;
-	    } else if (x > second && x < highest) {
-	      second = x;
-	    }
-	  }
-
-return second;
+	
 }
