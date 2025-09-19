863b9af22ab189c1797a616fc04f1f692b19a381
Aryan Kesarwani <93967099+Aryankesarwani@users.noreply.github.com>
2025-09-19 17:04:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+secondHighest(arr)=>{
+	let highest=arr[0];
+	let second=arr[0];
+	for(let x:arr){
+		if(x>highest){
+			second=highest;
+			highest=x;
+		}
+		else if(x>second){
+		second=x;
+		}
+	}
+
+return second;
+}
